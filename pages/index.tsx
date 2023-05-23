4b51075dfe8e28fc3727b9c8e125020b08fd49f3
mini <jma1020@naver.com>
2023-05-24 02:19:29 +0900

    Design : tailwind grid 및 각종 클래스 연습

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,28 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-slate-400 py-10 px-5 flex-col space-y-5">
-      <div className="bg-white p-10 rounded-2xl "> </div>
-      <div className="bg-white p-10 rounded-2xl"></div>
-      <div className="bg-white p-10 rounded-2xl"></div>
-      <div className="bg-white p-10 rounded-2xl"></div>
+    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+      <div className="bg-white p-6 rounded-3xl shadow-xl ">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between mb-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>total</span>
+          <span className="font-semibold">$38</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
+          checkout
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
   );
 }
