763de6a274a55e63c2758ce0a2b6d6a9c75deae5
mini <jma1020@naver.com>
2023-05-24 02:04:06 +0900

    Design : tailwind flex 연습

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,5 +4,12 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return null;
+  return (
+    <div className="bg-slate-400 py-10 px-5 flex-col space-y-5">
+      <div className="bg-white p-10 rounded-2xl "> </div>
+      <div className="bg-white p-10 rounded-2xl"></div>
+      <div className="bg-white p-10 rounded-2xl"></div>
+      <div className="bg-white p-10 rounded-2xl"></div>
+    </div>
+  );
 }
