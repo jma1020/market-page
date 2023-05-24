191378573cdfcb73f501af392274c2ba23707c3e
mini <jma1020@naver.com>
2023-05-24 10:56:14 +0900

    Design : 두 번째 profle card 스타일 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+    <div className="bg-slate-400 py-20 px-20 grid gap-10">
       <div className="bg-white p-6 rounded-3xl shadow-xl ">
         <span className="font-semibold text-3xl">Select Item</span>
         <div className="flex justify-between mb-2">
@@ -24,7 +24,28 @@ export default function Home() {
           checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <h3 className="text-white text-2xl">Profile</h3>
+        </div>
+        <div className="bg-white p-6 realtive -top-5 rounded-3xl">
+          <div className="relative -top-16 flex items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <p className="font-medium">340</p>
+            </div>
+            <div className="w-24 h-24  bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <p className="font-medium">340</p>
+            </div>
+          </div>
+          <div className="-mt-10  flex flex-col items-center ">
+            <p className="text-lg font-medium">Tony</p>
+            <span className="text-sm text-gray-500">한국</span>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
